3785f1256a24df42bfd1887de82052ae1133b45e
BeatriceStropeni <56863669+BeatriceStropeni@users.noreply.github.com>
2022-08-09 14:49:52 +0200

    Update prune_graph_edges.py

--- prune_graph_edges.py
+++ prune_graph_edges.py
@@ -6,10 +6,10 @@ import sys
 Prune edges from re-assembled graph to simplify the initial strain problem 
 '''
 
-# sys.argv[1]: Edge metadata CSV [id,length,contigs,reads,coverage_contigs,coverage_reads,sequence]
+# sys.argv[1]: Edge metadata CSV [id,length,contigs,reads,coverage_contigs,coverage_reads]
 
 # Extract edges to keep 
-edge_df = pd.read_csv(sys.argv[1], header = 0,sep = ';')
+edge_df = pd.read_csv(sys.argv[1], header = 0)
 edge_nums = list(edge_df['id'])
 edge_lst = ['edge_' + str(i) for i in edge_nums]
 prefix = join(*basename(sys.argv[1]).split('.')[:-1])
@@ -25,13 +25,28 @@ l_out = l_int[l_int[3].isin(edge_lst)]
 l_out.to_csv('L_pruned_' + prefix + '.tsv', header = False, index = False, sep = '\t')
 
 # Extract contigs to keep
-ctgs = list(edge_df['contigs'])
-ctg_lst = []
-for c in ctgs:
-	tmp = c.replace('"', '').replace('\'', '').replace('[', ''). replace(']', '').split(', ')
-	ctg_lst.extend(tmp)
+# ctgs = list(edge_df['contigs'])
+# ctg_lst = []
+# for c in ctgs:
+# 	tmp = c.replace('"', '').replace('\'', '').replace('[', ''). replace(']', '').split(', ')
+# 	ctg_lst.extend(tmp)
+
+def remove_edges(row, lst):
+	edges = row[2].split(',')
+	to_keep = []
+	for edge in edges:
+		tmp = edge
+		if tmp.replace('+', '').replace('-', '') in lst:
+			to_keep.append(edge)
+	if len(to_keep) > 1:
+		row[2] = ','.join(to_keep)
+		return row
+	else:
+		return
 
 # Make the path (contig) components
-p_df = pd.read_csv('P.tsv', header = None, sep = '\t')
-p_out = p_df[p_df[1].isin(ctg_lst)]
+p_df = pd.read_csv('P.tsv', header = None, sep = '\t') # P contig_X edge_Y,edge_Z,etc. *
+p_pruned = p_df.apply(lambda row : remove_edges(row, edge_lst), axis = 1)
+p_pruned.dropna(how = 'all', inplace = True)
+# p_out = p_df[p_df[1].isin(ctg_lst)]
 p_out.to_csv('P_pruned_' + prefix + '.tsv', header = False, index = False, sep = '\t')
